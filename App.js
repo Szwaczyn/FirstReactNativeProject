ba0361f7c36e20feb45d1a7de624aab7baed5c66
Szwaczyn <michal.szwaczyk@outlook.com>
2020-10-09 19:57:33 +0200

    Scrollable list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput} from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, ScrollView} from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('Michal');
@@ -10,6 +10,15 @@ export default function App() {
     age: 21,
   });
 
+  const [people, setPeople] = useState([
+    { key: 1, name: 'Jan', age: 32} ,
+    { key: 2, name: 'Roman', age: 12 },
+    { key: 3, name: 'Kasjusz', age: 54 },
+    { key: 4, name: 'Tadeusz', age: 12 },
+    { key: 5, name: 'Leszek', age: 72 },
+    { key: 6, name: 'Abraham', age: 65 },
+  ])
+
   const clickHandler = () => {
 
       setName('Roman');
@@ -18,6 +27,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      {/* Basics */}
       <View style={styles.header}>
         <Text style={styles.boldText}>Hi, ia am {name} and my age is {age}</Text>
         <Text style={styles.boldText}>His name is {person.name} and his age is {person.age}</Text>
@@ -29,7 +39,7 @@ export default function App() {
         style={styles.input} 
         placeholder='e.g. John Doe' 
         onChangeText={(value) => setName(value)}/>
-        
+
       <Text>Write down your age</Text>
       <TextInput 
         keyboardType='numeric'
@@ -43,6 +53,19 @@ export default function App() {
         <Button title='Update me' onPress={clickHandler} />
       </View>
       <StatusBar style="auto" />
+
+    {/* Lists and scrols view */}
+    
+      <ScrollView>
+      { people.map((item) => {
+        return (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )
+        
+      })}
+    </ScrollView>
     </View>
   );
 }
@@ -52,7 +75,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f1f1f1',
     alignItems: 'center',
-    justifyContent: 'center',
+//    justifyContent: 'center',
   },
   header: {
     backgroundColor: '#A30AB1',
@@ -72,4 +95,10 @@ const styles = StyleSheet.create({
     margin: 10,
     width: 200,
   },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'green',
+    fontSize:24,
+  },  
 });
